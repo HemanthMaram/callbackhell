69122c04d68cb88b45feef6bd947de0d89ce7861
HemanthMaram <83005267+HemanthMaram@users.noreply.github.com>
2021-07-11 14:31:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let display  = (num)=>{
+   let p = document.createElement('p')
+   p.innerHTML=num;
+   p.style.textAlign='center'
+   
+   p.style.color='blue'
+   document.body.append(p)
+}
+display('Happy Birthday'
+   ,
+   display(1,
+      display(2,
+         display(3,
+            display(4,
+               display(5,
+                  display(6,
+                     display(7,
+                        display(8,
+                           display(9,
+                              display(10))))))
+               )))))
+ 
+      
+  
+
+
